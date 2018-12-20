2531443811fa50f917b33f1cb1b9851baf4b2ef2
Your Name <you@example.com>
2018-12-20 22:20:11 +0300

    Updates

---
+++ precache-manifest.805c8b9635f610cd8a40c66efae02fcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "deb37338b445e90e72fa",
+    "url": "/itra-courses-hw-7/static/css/main.02d89891.chunk.css"
+  },
+  {
+    "revision": "deb37338b445e90e72fa",
+    "url": "/itra-courses-hw-7/static/js/main.deb37338.chunk.js"
+  },
+  {
+    "revision": "846e098a801d8cb491f7",
+    "url": "/itra-courses-hw-7/static/js/1.846e098a.chunk.js"
+  },
+  {
+    "revision": "505869b871151a7244c1",
+    "url": "/itra-courses-hw-7/static/js/runtime~main.505869b8.js"
+  },
+  {
+    "revision": "f7d728f2a3cfb5eede145f622d5b6e2e",
+    "url": "/itra-courses-hw-7/index.html"
+  }
+];
